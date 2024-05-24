dd5ea33a3a139876e39f83c09171f1fd8ba4f6e9
Manish Chhetri <chhetridon123@gmail.com>
2024-05-24 23:45:31 +0545

    Refactor file paths and update dependencies in package.json

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -4,10 +4,14 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
   blog: {
     type: String,
     required: true,
   },
 });
 
-module.exports = mongoose.model("Blog", userSchema, "Blog");
+module.exports = mongoose.model("User", userSchema, "users");
